38a8cfc4a8588c107f110ed013b3f68f2726b4f5
gonzajaimes <gonzaiames@hotmail.com>
2018-11-03 23:59:06 -0500

    Alertas para login y password

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -2,12 +2,14 @@
 
 namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
     using System.Text;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-   public class LoginViewModel
+    public class LoginViewModel
     {
         #region Properties
         public string Email { get; set; }
@@ -24,7 +26,36 @@ namespace Lands.ViewModels
         }
         #endregion
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+           
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+
+            }
+        }
         #endregion
     }
 }
