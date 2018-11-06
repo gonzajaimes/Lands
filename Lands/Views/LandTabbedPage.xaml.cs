b84b19884842cc867c3b5dd1f21d7477072066a0
gonzajaimes <gonzajaimes@hotmail.com>
2018-11-06 16:01:24 -0500

    Lands Tabbed Page and Icons

---
+++ Lands/Views/LandTabbedPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Lands.Views
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Xamarin.Forms;
+
+    public partial class LandTabbedPage : TabbedPage
+    {
+        public LandTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lands/Views/LanguagesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Lands.Views
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Xamarin.Forms;
+
+    public partial class LanguagesPage : ContentPage
+    {
+        public LanguagesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
