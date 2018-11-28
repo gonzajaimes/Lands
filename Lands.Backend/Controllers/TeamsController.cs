ab1fe83e58868ebed07b92cef50f64d449c9b5f3
gjaimes <gjaimes@emeingenieria.com.co>
2018-11-28 16:24:09 -0500

    Teams Backend

---
+++ Lands.Backend/Controllers/TeamsController.cs
@@ -0,0 +1,148 @@
+﻿
+
+namespace Lands.Backend.Controllers
+{
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+    using System.Net;
+    using System.Web.Mvc;
+    using Models;
+    using Lands.Domain;
+    using Helpers;
+
+    [Authorize(Roles = "Admin") ]
+    public class TeamsController : Controller
+    {
+        private LocalDataContext db = new LocalDataContext();
+
+        // GET: Teams
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Teams.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = await db.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TeamView view)
+        {
+            if (ModelState.IsValid)
+            {
+                var pic = string.Empty;
+                var folder = "~/Content/Teams";
+
+                if (view.ImageFile != null)
+                {
+                    pic = FilesHelper.UploadPhoto(view.ImageFile, folder);
+                    pic = string.Format("{0}/{1}", folder, pic);
+                }
+
+                var team = this.ToTeam(view);
+                team.ImagePath = pic;
+                db.Teams.Add(team);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(view);
+        }
+
+        private Team ToTeam(TeamView view)
+        {
+            return new Team
+            {
+                ImagePath = view.ImagePath,
+                Name = view.Name,
+                TeamId = view.TeamId,
+            };
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = await db.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "TeamId,Name,ImagePath")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(team).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = await db.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Team team = await db.Teams.FindAsync(id);
+            db.Teams.Remove(team);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
